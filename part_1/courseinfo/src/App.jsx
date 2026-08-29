cccfc7d2c6ed92a7b1f4d71941f84ea3e726c9f0
Samuel Bilek <sam@polygoniq.com>
2026-08-29 17:49:36 +0200

    Exercise 1.3

--- part_1/courseinfo/src/App.jsx
+++ part_1/courseinfo/src/App.jsx
@@ -34,18 +34,24 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises_count: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises_count: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises_count: 14
+  }
 
   return (
     <>
       <Header course_name={course} />
-      <Content part1={{ name: part1, exercises_count: exercises1 }} part2={{ name: part2, exercises_count: exercises2 }} part3={{ name: part3, exercises_count: exercises3 }} />
-      <Total exercises_count={exercises1 + exercises2 + exercises3} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total exercises_count={part1.exercises_count + part2.exercises_count + part3.exercises_count} />
     </>
   )
 }
