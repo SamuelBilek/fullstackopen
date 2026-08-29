727c5579886d785e98de7381b55a40e67b339a9c
Samuel Bilek <sam@polygoniq.com>
2026-08-29 17:59:39 +0200

    Exercise 1.4

--- part_1/courseinfo/src/App.jsx
+++ part_1/courseinfo/src/App.jsx
@@ -9,7 +9,7 @@ const Header = (props) => {
 const Part = (props) => {
   return (
     <div>
-      <p>{props.part_name} {props.exercises_count}</p>
+      <p>{props.part.name} {props.part.exercises_count}</p>
     </div>
   )
 }
@@ -17,9 +17,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part_name={props.part1.name} exercises_count={props.part1.exercises_count} />
-      <Part part_name={props.part2.name} exercises_count={props.part2.exercises_count} />
-      <Part part_name={props.part3.name} exercises_count={props.part3.exercises_count} />
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </div>
   )
 }
@@ -27,31 +27,33 @@ const Content = (props) => {
 const Total = (props) => {
   return (
     <div>
-      <p>Number of excersices {props.exercises_count}</p>
+      <p>Number of excersices {props.parts[0].exercises_count + props.parts[1].exercises_count + props.parts[2].exercises_count}</p>
     </div>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises_count: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises_count: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises_count: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises_count: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises_count: 7
+    },
+    {
+      name: 'State of a component',
+      exercises_count: 14
+    }
+  ]
 
   return (
     <>
       <Header course_name={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total exercises_count={part1.exercises_count + part2.exercises_count + part3.exercises_count} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </>
   )
 }
