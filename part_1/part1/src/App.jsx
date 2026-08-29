761d4cdeea74a415763db1b8125438e35d027919
Samuel Bilek <sam@polygoniq.com>
2026-08-29 19:42:24 +0200

    Tutorial 1c

--- part_1/part1/src/App.jsx
+++ part_1/part1/src/App.jsx
@@ -1,29 +1,37 @@
-const Hello = (props) => {
+import { useState } from 'react'
 
-  console.log(props)
-  return (
-    <div>
-      <p>
+const Display = ({counter}) => <div>{counter}</div>
 
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  )
-}
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
+  const [ counter, setCounter ] = useState(0)
+  console.log('rendering with counter value', counter)
 
-  const name = 'Peter'
-  const age = 10
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
 
   return (
     <div>
-      <h1>Greetings</h1>
-
-      <Hello name='Maya' age={26 + 10} />
-      <Hello name={name} age={age} />
+      <Display counter={counter} />
+        <Button text='plus' onClick={increaseByOne} />
+        <Button text='zero' onClick={setToZero} />
+        <Button text='minus' onClick={decreaseByOne} />
     </div>
   )
 }
+
 
 export default App
