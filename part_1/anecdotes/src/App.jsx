322b728f547807e046a4ab61d1212134b6de50c9
Samuel Bilek <sam@polygoniq.com>
2026-08-30 18:00:54 +0200

    Exercises 1.12 and 1.13

---
+++ part_1/anecdotes/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+
+const getRandomInt = (max, exclude) => {
+  let ret = 0
+  do {ret = Math.floor(Math.random() * max)} 
+  while (ret === exclude)
+  return ret;
+}
+
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(getRandomInt(anecdotes.length - 1))
+  const [votes, setVotes] = useState({})
+
+  const voteCurrentAnecdote = () => {
+    let updatedVotes = {...votes}
+    let vote = selected.toString()
+    let currentValue = updatedVotes[vote]
+    updatedVotes[vote] = currentValue ? currentValue + 1 : 1
+    setVotes(updatedVotes)
+  }
+
+  return (
+    <div>
+      <p>
+        {anecdotes[selected]}
+        <br></br>
+        has {votes[selected] || 0} votes
+      </p>
+      <Button onClick={voteCurrentAnecdote} text='vote' />
+      <Button onClick={() => setSelected(getRandomInt(anecdotes.length - 1, selected))} text='next anecdote' />
+    </div>
+  )
+}
+
+export default App
