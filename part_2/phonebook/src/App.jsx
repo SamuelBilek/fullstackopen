f6d0139c8cd6dcb33475e335ff4821a877eb3c15
Samuel Bilek <sam@polygoniq.com>
2026-08-30 21:35:34 +0200

    Exercise 2.8

--- part_2/phonebook/src/App.jsx
+++ part_2/phonebook/src/App.jsx
@@ -2,34 +2,40 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    {name: 'Arto Hellas', number: '040-1234567'}
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const isDuplicateName = (name) => {
     return persons.map(person => person.name).includes(name)
   }
 
-  const handleNameSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault()
     if (isDuplicateName(newName)) {
       alert(`${newName} is already added to the phonebook`)
       setNewName('')
       return
     }
-    setPersons(persons.concat({name: newName}))
+    setPersons(persons.concat({name: newName, number:newNumber}))
     setNewName('')
+    setNewNumber('')
   }
 
-  const NameLabel = ({text}) => {
+  const NameLabel = ({person}) => {
     return (
       <>
         <label>
-          {text}
+          {person.name} {person.number}
         </label>
         <br/>
       </>
@@ -42,13 +48,15 @@ const App = () => {
       <form>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
+          <br/>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
-          <button type="submit" onClick={handleNameSubmit}>add</button>
+          <button type="submit" onClick={handleSubmit}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <NameLabel key={person.name} text={person.name} />)}
+      {persons.map(person => <NameLabel key={person.name} person={person} />)}
     </div>
   )
 }
