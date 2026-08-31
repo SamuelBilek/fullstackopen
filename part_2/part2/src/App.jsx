2074158f4bc768118dc3081a8baa3900e0baa52e
Samuel Bilek <sam@polygoniq.com>
2026-08-31 07:43:42 +0200

    Tutorial 2c

--- part_2/part2/src/App.jsx
+++ part_2/part2/src/App.jsx
@@ -1,10 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }
+
+  useEffect(hook, [])
+
+  console.log('render', notes.length, 'notes')
 
   const addNote = (event) => {
     event.preventDefault()
@@ -24,7 +39,7 @@ const App = (props) => {
   }
 
   const notesToShow = showAll ? notes : notes.filter(note => note.important)
-
+  
   return (
     <div>
       <h1>Notes</h1>
