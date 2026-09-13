c2c5e50d9ddae6c27f3fdd72d66c38c8ab1a02d2
Samuel Bilek <sam@polygoniq.com>
2026-09-13 15:48:30 +0200

    Tutorial 3b

--- part_3/backend/index.js
+++ part_3/backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static('dist'))
+
 const requestLogger = (request, response, next) => {
     console.log('Method: ', request.method)
     console.log('Path:  ', request.path)
@@ -90,6 +92,6 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
